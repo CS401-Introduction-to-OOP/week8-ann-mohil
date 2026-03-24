d660eae5619a64d7a02dc91dc86fc98956e9f007
ann-mohil <anna.mohil.25@kse.org.ua>
2026-03-24 19:19:07 +0200

    level 1

--- Week8/DeliveryItem.cs
+++ Week8/DeliveryItem.cs
@@ -1,6 +1,20 @@
 namespace Week8;
 
-public class DeliveryItem
+public abstract class DeliveryItem
 {
-    
+    public string TrackingNumber { get; }
+    public double Weight { get; }
+
+    protected DeliveryItem(string trackingNumber, double weight)
+    {
+        TrackingNumber = trackingNumber;
+        Weight = weight;
+    }
+
+    public abstract double CalculateCost();
+
+    public virtual void PrintInfo()
+    {
+        Console.WriteLine($"Delivery Item : {TrackingNumber} Weight : {Weight} kg");
+    }
 }
