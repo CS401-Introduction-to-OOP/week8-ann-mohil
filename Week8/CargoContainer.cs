6b1132ed8e0e02ba4b3996396a64790bf68b48d7
ann-mohil <anna.mohil.25@kse.org.ua>
2026-03-24 19:19:19 +0200

    level 2

--- Week8/CargoContainer.cs
+++ Week8/CargoContainer.cs
@@ -1,6 +1,22 @@
 namespace Week8;
 
-public class CargoContainer
+public class CargoContainer<T> where T : DeliveryItem
 {
-    
+    private List<T> _items = new List<T>();
+
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+        Console.WriteLine($"Added: {item.TrackingNumber}");
+    }
+
+    public double GetTotalCost()
+    {
+        double result = 0;
+        foreach (var item in _items)
+        {
+            result += item.CalculateCost();
+        }
+        return result;
+    }
 }
